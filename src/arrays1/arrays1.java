203594ecbec38e94f21b18eb2f0d155097e8d6c5
Vishwanath Manvi <vishu@Vishwanaths-MacBook-Pro.local>
2018-06-26 00:22:42 -0700

    arrays program

---
+++ src/arrays1/arrays1.java
@@ -0,0 +1,18 @@
+package arrays1;
+
+public class arrays1 {
+
+	public static void main(String args[]) {
+		int []numbers;
+		numbers = new int[1000];
+		for (int i=0; i<1000; i++) {
+			do {
+				numbers[i]= (int)((Math.random()*1000)%100);
+			} while (numbers[i] < 10);
+			
+			if (i%20 == 0)
+				System.out.print("\n");
+			System.out.print(numbers[i] + " ");
+		}
+	}
+}
